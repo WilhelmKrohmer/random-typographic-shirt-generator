1ead5d2c368bf9743fc3bfd522f1f9574dd11071
WilhelmKrohmer <w.g.krohmer@gmail.com>
2019-04-26 20:22:34 +0200

    add function to set random verb values

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+/**
+ * These variables contain the text that needs to be changed and 
+ * the button that executes the changes.
+ *  */
 let verb = document.getElementById('verb');
 let adjective = document.getElementById('adjective');
-let generateButton = document.getElementById('generate-btn');
+const generateButton = document.getElementById('generate-btn');
 
-generateButton.click('generateText');
+/**
+ * This array contains all the verbs, that can be applied to the t-shirt.
+ *  */ 
+let verbList = ["Be", "Go", "Do"];
+console.log(verbList);
 
+generateButton.addEventListener('click', generateText);
+
+/**
+ * This function is triggered when the generateButton is clicked.
+ */
 function generateText() {
-    
+    setVerb();
+}
+
+function setVerb() {
+    let randomVerbNumber = Math.floor((Math.random() * verbList.length) + 0);
+    verb.textContent = verbList[randomVerbNumber];
 }
