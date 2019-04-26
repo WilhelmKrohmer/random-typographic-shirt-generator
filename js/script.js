3273f5c3cd4ec2787fc972ac112c6c9f76c879a3
WilhelmKrohmer <w.g.krohmer@gmail.com>
2019-04-26 19:17:50 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let verb = document.getElementById('verb');
+let adjective = document.getElementById('adjective');
+let generateButton = document.getElementById('generate-btn');
+
+generateButton.click('generateText');
+
+function generateText() {
+    
+}
